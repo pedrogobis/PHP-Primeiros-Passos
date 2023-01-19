b5478643dc3e38288d027a79974c6e0e56a17884
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2023-01-19 16:43:27 -0300

    Include e required

---
+++ PHP-Curso2/clientes.php
@@ -0,0 +1,18 @@
+<?php
+
+$contasCorrentes = [
+    '13212312332' => [
+        'Titualar' => 'Vinicios',
+        'Saldo' => '2340.49'
+    ],
+    '12312333221'=>[
+        'Titualar' => 'Claudio',
+        'Saldo' => '2560.45'
+    ],
+    '12344333455'=>[
+        'Titualar' => 'Marcela',
+        'Saldo' => '498,00'
+    ]
+
+];
+
---
+++ PHP-Curso2/funcoes.php
@@ -0,0 +1,27 @@
+<?php
+
+function exibeMenssagem(string $mensagem){
+    echo $mensagem . PHP_EOL;
+};
+
+function sacar(array $conta, float $valorsacar): array 
+{
+    if($valorsacar > $conta['Saldo']){
+        exibeMenssagem("Você não pode sacar!");
+    }else{
+        $conta['Saldo'] -= $valorsacar;
+        
+    }
+    return $conta;
+};
+
+function depositar(array $conta,float $valordepositar): array
+{
+    if($valordepositar> 0){
+        $conta['Saldo'] += $valordepositar;
+    }else{
+        exibeMenssagem("Valor invalido");
+    }
+    
+    return $conta;
+};
